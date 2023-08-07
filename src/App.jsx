4417602ab28c5f0504a6ace8f36424a7f37734f7
rajbeer1 <103877522+rajbeer1@users.noreply.github.com>
2023-08-07 14:03:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const getsongs = async () => {
     try {
       const response = await axios.post(
-        'https://melomagic-api.onrender.com/playlist',
+        'ec2-3-80-211-234.compute-1.amazonaws.com:4500/playlist',
         {
           limit: limit,
           target_liveness: target_liveness,
